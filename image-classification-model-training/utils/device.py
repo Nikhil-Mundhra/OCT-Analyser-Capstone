51cd0ba22bedb3a4c609d0bf096e68a20086e1d6
Nikhil Mundhra <nikhilmundhra28@gmail.com>
2026-08-01 13:42:24 +0530

    fix(classification): enable find_unused_parameters=True in DistributedDataParallel for frozen backbone warmup phase

--- image-classification-model-training/utils/device.py
+++ image-classification-model-training/utils/device.py
@@ -116,7 +116,7 @@ class ComputeManager:
                 model,
                 device_ids=[self.local_rank],
                 output_device=self.local_rank,
-                find_unused_parameters=False,
+                find_unused_parameters=True,
                 gradient_as_bucket_view=False
             )
         elif self.use_data_parallel:
